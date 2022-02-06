2482b04e09f80fa02e11e9faf4152060e90681d2
jyoti patel <jyotipatelfyit77100@gmail.com>
2022-02-06 23:48:21 +0530

    edit react files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className=' bg-[#fff] m-auto max-w-xl w-full h-[100vh]'>
+        <div className=' absolute right-0'>
+          <button className='flex m-2'>
+            random
+            <i className='material-icons ml-2'>cached</i>
+          </button>
+        </div>
+        <p className=''>
+          “The first rule of any technology used in a business is that
+          automation applied to an efficient operation will magnify the
+          efficiency. The second is that automation applied to an inefficient
+          operation will magnify the inefficiency.”
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className=''>
+          <h1 className=''>Bill Gates</h1>
+          <p className=''>business</p>
+        </div>
+
+        <footer className='absolute bottom-0  '>
+          <p className='text-center'>
+            created by{' '}
+            <a href='' target='_blank' rel='noopener noreferrer'>
+              jyoti-p
+            </a>
+            - devChallenges.io
+          </p>
+        </footer>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
